a247ec66ef2178ae265a5d3c67e80e452ea055cd
Tomas-Paronai <tomas.paronai@akademiasovy.sk>
2016-02-29 14:06:51 +0100

    game

--- app/src/main/java/parohyapps/cardcounter/PlayActivity.java
+++ app/src/main/java/parohyapps/cardcounter/PlayActivity.java
@@ -1,13 +1,84 @@
 package parohyapps.cardcounter;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import parohyapps.cardcounter.core.Game;
 
 public class PlayActivity extends AppCompatActivity {
+
+    private Game game;
+    private int result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        game = new Game();
+        generateGameState();
+    }
+
+    private void generateGameState(){
+        ImageView leftView = (ImageView) findViewById(R.id.iw_left_card);
+        ImageView rightView = (ImageView) findViewById(R.id.iw_right_card);
+
+        leftView.setImageDrawable(ContextCompat.getDrawable(this,getResources().getIdentifier(game.getLeftCard(),"drawable",getPackageName())));
+        rightView.setImageDrawable(ContextCompat.getDrawable(this,getResources().getIdentifier(game.getRightCard(),"drawable",getPackageName())));
+
+        int leftNumber = game.getLeftCardNumber();
+        int rightNumber = game.getRightCardNumber();
+
+        if(game.isLeftNegative()){
+            leftNumber *= -1;
+        }
+        if(game.isRightNegative()){
+            rightNumber *= -1;
+        }
+
+        result = leftNumber + rightNumber;
+
+        Log.d("Generated Game","Left: "+leftNumber);
+        Log.d("Generated Game","Right: "+rightNumber);
+        Log.d("Generated Game","Result: "+result);
+
+        TextView leftHelper = (TextView) findViewById(R.id.tw_left_helper);
+        TextView rightHelper = (TextView) findViewById(R.id.tw_right_helper);
+        leftHelper.setText(String.valueOf(leftNumber));
+        rightHelper.setText(String.valueOf(rightNumber));
+
+        EditText userInput = (EditText) findViewById(R.id.et_user_input);
+        userInput.setText("");
+    }
+
+    public int getResult(){
+        return result;
+    }
+
+    public void confirmResult(View v){
+        EditText userInput = (EditText) findViewById(R.id.et_user_input);
+        TextView gameMessage = (TextView) findViewById(R.id.game_message);
+        if(!userInput.getText().toString().equals("")){
+            if(userInput.getText().toString().equals(String.valueOf(getResult()))){
+                gameMessage.setText(getResources().getIdentifier("result_ok","string",getPackageName()));
+            }
+            else{
+                gameMessage.setText(getResources().getIdentifier("result_wrong","string",getPackageName()));
+            }
+            generateGameState();
+        }
+    }
+
+    public void helpMe(View v){
+        TextView leftHelper = (TextView) findViewById(R.id.tw_left_helper);
+        TextView rightHelper = (TextView) findViewById(R.id.tw_right_helper);
+        leftHelper.setVisibility(View.VISIBLE);
+        rightHelper.setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/parohyapps/cardcounter/core/Game.java
@@ -0,0 +1,113 @@
+package parohyapps.cardcounter.core;
+
+import java.util.Random;
+
+/**
+ * Created by tomas on 2/29/2016.
+ */
+public class Game {
+
+    public static int SPADES = 0;
+    public static int DIAMONDS = 1;
+    public static int HEARTS = 2;
+    public static int CLUBS = 3;
+
+    private boolean left;
+    private int leftCard;
+    private int rightCard;
+    private boolean leftNegative;
+    private boolean rightNegative;
+
+    public String getLeftCard(){
+        leftCard = randInt(1,13);
+        String result = "c"+getCardTypeString(leftCard)+"_of_"+getTypeString(randInt(0,3));
+        return result;
+    }
+
+    public String getRightCard(){
+        rightCard = randInt(1,13);
+        String result = "c"+getCardTypeString(rightCard)+"_of_"+getTypeString(randInt(0,3));
+        return result;
+    }
+
+    public int getLeftCardNumber(){
+        return leftCard;
+    }
+
+    public int getRightCardNumber(){
+        return rightCard;
+    }
+
+    public boolean isLeftNegative(){
+        return leftNegative;
+    }
+
+    public boolean isRightNegative(){
+        return rightNegative;
+    }
+
+    private String getCardTypeString(int number){
+        if(number > 1 && number < 11){
+            return String.valueOf(number);
+        }
+        else{
+            if(number == 11){
+                return "jack";
+            }
+            else if(number == 12){
+                return "queen";
+            }
+            else if(number == 13){
+                return "king";
+            }
+            else{
+                return "ace";
+            }
+        }
+    }
+
+    private String getTypeString(int type){
+        setNegatives(type);
+        if(type == SPADES){
+            return "spades";
+        }
+        else if(type == DIAMONDS){
+
+            return "diamonds";
+        }
+        else if(type == HEARTS){
+            return "hearts";
+        }
+        else{
+            return "clubs";
+        }
+    }
+
+    private void setNegatives(int type){
+        if(type == DIAMONDS || type == HEARTS){
+            if(left){
+                leftNegative = true;
+                left = false;
+            }
+            else{
+                rightNegative = true;
+                left = true;
+            }
+        }
+        else{
+            if(left){
+                leftNegative = false;
+                left = false;
+            }
+            else{
+                rightNegative = false;
+                left = true;
+            }
+        }
+    }
+
+    public int randInt(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
+}
